b69a50607ada1b51cd6c1b946018f840f54056e5
ivan321 <ivanbutler@gmail.com>
2013-02-07 22:50:27 +0000

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
-  
+  before_save :create_remember_token
 
   
   validates :name, :presence => true, :length => {:maximum => 25}
@@ -20,5 +20,11 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, :format => {:with => VALID_EMAIL_REGEX}, :uniqueness => {:case_sensitive => false}
   validates :password, :presence => true, :length => { :minimum => 6 }
   validates :password_confirmation, :presence => true
+
+
+   def create_remember_token
+   	self.remember_token = SecureRandom.hex
+   	end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Nice::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   #get "static_pages/home"
 
@@ -16,6 +17,8 @@ Nice::Application.routes.draw do
   match '/about', :to => 'static_pages#about'
   match '/contact', :to => 'static_pages#contact'
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy', :via => :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
